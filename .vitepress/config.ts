def9a2459307c4f478e4d60ab9e0b13466d0a712
albertlast <mathiaspapealbert@hotmail.com>
2023-04-20 20:34:06 +0200

    change base path

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   title: "SAP-SuccessFactors Limitation",
   description: "A collection of Limitation of SuccessFactors",
   lang: "en",
+  base: "./",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
